d78ce383084258b8a1aab0a991fc0402834354e8
Danny Harding <dannyharding10@gmail.com>
2017-03-15 00:06:24 -0600

    usernames

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ io.on('connection', function(socket){
   console.log('a user connected');
 
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  	socket.broadcast.emit('chat message', msg);
   });
 
   socket.on('disconnect', function(){
